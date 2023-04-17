db6da108fb9b62e71485748d4358f9bfe25cf850
CarlCort <124218592+CarlCort@users.noreply.github.com>
2023-04-17 18:10:59 -0400

    Added subscriber that subscribes to the 'aruco_detections' topic and stores detected aruco tags.
    I added subscriber that subscribes to the 'aruco_detections' topic and stores detected aruco tags. I figured that a good way to implement this would be with a queue implemented as a list in python. Since this is an inline script (not a class), I simply made both the list and the subscribers global variables. The list can be accessed within a given scope by defining it locally as the global variable using the global keyword. The best way to finish this implementation will likely be to have a "act according to tags" method of some sore that is simply constantly run that will retrieve the first not-yet-handled aruco tag from the global list and then call the correct movement method to execute the appropriate action. After that it will remove it from the list and move on to the next tag in the list, if there are any.

--- ros/aruco_tag/script/movement_test.py
+++ ros/aruco_tag/script/movement_test.py
@@ -2,10 +2,22 @@ import rospy
 import cv2
 import math
 from geometry_msgs.msg import Twist
+from std_msgs.msg import int16
 
 # Initialize publisher to control the TurtleBot's movement
 velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+# Creates a subscriber that recieves an int16 (simple message from the std_msgs package that contains one 16-bit int, which is all that is needed)
+aruco_detect_subscriber = rospy.Subscriber('aruco_detections', int16, aruco_detection_callback)
+# defines and initializes an empty list that will store the detected tags that have not yet been handled. I assume that we will want to effectively treat it as
+# a queue and add and remove things in first-in first-out (FIFO) order.
+detected_tags = []
 
+# This is a simple callback that will be called by the rosmaster whenever a new tag is detected and published on the aruco_detections topic. It is saved in the
+# detected_tags global set. The intention for this set is that when there is an aruco tag detected it is added to the set. Once that tag has been executed, it will be removed.
+# data will be a 16-bit int
+def aruco_detection_callback(data):
+    detected_tags.add(data)
+    
 def move_forward(distance_to_move, velocity_publisher):
     # Create a Twist message to control the TurtleBot's movement
     velocity_msg = Twist()
@@ -78,3 +90,4 @@ if __name__ == '__main__':
     rospy.init_node('movement_tester', anonymous=True)
     rospy.on_shutdown(shutdown)
     move_forward(1.0,velocity_publisher)
+   
